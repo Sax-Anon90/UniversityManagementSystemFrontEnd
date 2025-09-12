5f8712a1be11b339fe532c346596e187270db67c
Sax <42096318+Sax-Anon90@users.noreply.github.com>
2025-09-12 14:56:22 +0200

    Add: Radzen UI + Layout Setup using Radzen components

--- UniversityManagementSystem.BlazorWASM/Client/Program.cs
+++ UniversityManagementSystem.BlazorWASM/Client/Program.cs
@@ -1,5 +1,7 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using UniversityManagementSystem.BlazorWASM.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddRadzenComponents();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+
 
 await builder.Build().RunAsync();
+
